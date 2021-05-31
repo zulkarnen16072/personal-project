783a17dde3eb04e56f4b7c03b12f57149c55fc62
Zulkarnen <zulkarnen1229@gmail.com>
2021-05-31 18:05:48 +0700

    Generate Component Product

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule
