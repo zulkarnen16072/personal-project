06730d648ce66264d41ded86167b7b06019e66ad
Zulkarnen <zulkarnen1229@gmail.com>
2021-06-01 07:26:01 +0700

    Add Component public/shop

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {
@@ -13,7 +14,8 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
     CommonModule,
