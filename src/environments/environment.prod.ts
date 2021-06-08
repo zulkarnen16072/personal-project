fc4999daddc455587f7b9906f61b6ba7d11cbb1b
Zulkarnen <zulkarnen1229@gmail.com>
2021-06-08 22:45:26 +0700

    Konfigurasi Env Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCKHRftGxf8_ID9Epk9m2f-Qwb1KFeQilY",
+    authDomain: "personal-project-tekweb.firebaseapp.com",
+    projectId: "personal-project-tekweb",
+    storageBucket: "personal-project-tekweb.appspot.com",
+    messagingSenderId: "1029326080042",
+    appId: "1:1029326080042:web:edbe2eb5c5d6da01ac1b53"
+  }
+}
