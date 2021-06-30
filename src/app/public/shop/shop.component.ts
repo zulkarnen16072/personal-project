cb04f84a7ab8f43e102b8767496bf351cb2df998
Zulkarnen <zulkarnen1229@gmail.com>
2021-06-30 20:48:50 +0700

    Update Label to English on Product Detail

--- src/app/public/shop/shop.component.ts
+++ src/app/public/shop/shop.component.ts
@@ -52,6 +52,22 @@ export class ShopComponent implements OnInit {
       });
   }
 
+  formatRupiah(angka)
+  {
+    
+    var	number_string = angka.toString(),
+      sisa 	= number_string.length % 3,
+      rupiah 	= number_string.substr(0, sisa),
+      ribuan 	= number_string.substr(sisa).match(/\d{3}/g);
+        
+    if (ribuan) {
+      var separator = sisa ? '.' : '';
+      rupiah += separator + ribuan.join('.');
+    }
+
+    return rupiah
+  }
+
   
 
  
